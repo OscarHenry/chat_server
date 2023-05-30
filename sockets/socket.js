b61f32844a3b7204e8b3ae5898249364b9369bd0
Oscar Henry Torres Caballero <oscar.torres@lighthousetech.io>
2023-05-29 20:51:20 -0400

    first commit

---
+++ sockets/socket.js
@@ -0,0 +1,11 @@
+const {io} = require('../index');
+// Mensajes de sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+    client.on('disconnect', () => { console.log('Cliente desconectado'); });
+
+    client.on('mensaje',(payload)=>{
+        console.log('Mensaje recibido ',payload);
+        io.emit('mensaje',{admin:'He recibido un mensaje'});
+    });
+  });
